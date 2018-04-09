6e9790132c51ae86831f3f999ce79ac731e76906
Yannick Stoffers <y.stoffers@rug.nl>
2018-04-09 17:34:45 +0200

    Added the form for sign ups, senior students form still needs to be added. Also linking to the backend should be added.

---
+++ boilerplate_local/index-oud.php
@@ -0,0 +1,302 @@
+<?php
+
+header('Cache-control: private'); // IE 6 FIX
+
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" type="text/css" href="css/stylesheet.css">
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+
+		<script type="text/javascript" src="js/bootstrap.min.js"></script>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+
+		<title>
+			Introduction Camp 2017
+		</title>
+	</head>
+	<body data-spy="scroll" data-target="#navbar-swag">
+		<nav class="navbar navbar-default navbar-fixed-top" id="navbar-swag">
+			<div class="container">
+			<!-- Brand and toggle get grouped for better mobile display -->
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="http://svcover.nl">
+					<img src="images/cover_logo.png" alt="Cover logo">
+				</a>
+			</div>
+
+				<!-- Collect the nav links, forms, and other content for toggling -->
+				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+					<ul class="nav navbar-nav">
+						<li><a href="#home">Heroes VS Villians</a></li>
+						<li><a href="#commissie">Organisation</a></li>
+						<li><a href="#paklijst">Grablist</a></li>
+						<li><a href=
+						"#contact">Contact</a></li>
+					</ul>
+					<form class="navbar-form navbar-right">
+						<a href="#inschrijvingModal" class="btn btn-danger" data-toggle="modal">Sign Up!</a>
+					</form>
+				</div><!-- /.navbar-collapse -->
+			</div><!-- /.container-fluid -->
+		</nav>
+		<div class="container">
+			<div class="row navPadding" id="home">
+				<hr>
+				<h1>The Camp</h1>
+				<div class="row">
+					<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+						<img src="images/logo_colors.png" style="width:100%;" alt="Introcee logo">
+					</div>
+					<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+						<p class="pad">Welcome to the wonderfull website of  the Introdutory Committiee of 2017 of Cover. Are you starting the Bachelor Artificial Intelligence or Computing Science at the University of Groningen in 2017? Join the introductory camp organised by study association Cover! This is the opportunity to get to know your fellow students in a fun way and to make some new friends. We, the Introduction Committee, are really proud to organise this year's camp for you from the 8th till the 10th of September.
+<br />
+<br />
+Throughout the entire weekend there will be a fun programme full of nice activities to meet a lot of new people (both first years and a bunch of senior students). What's more is that there is plenty of time to do just about anything else and have fun with your new friends or play some cards. In other words, spending the first weekend as a student at this camp, (with your fellow first years and a some seniors) is the best way to get a head start in your new life as a student!
+						</p>
+					</div>
+				</div>
+				<hr>
+				<div class="row">
+					<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 pad">
+						<h3 class="pad">Location</h3>
+						<p class="pad">
+							This introductory camp will take place at a well-suited accommodation called <a href="http://www.pagedal.nl/nl/Groepsaccommodatie/Home">Pagedal</a> in the tiny town near Groningen called Stadskanaal, which itself is situated at the border between Groningen and Drenthe. Our departure from the university will be on Friday afternoon at 15:00, at which time a bus will take us straight to the campsite. As we made sure that you are completely free that Friday afternoon, you won't have to worry about missing any classes. Sometime Sunday afternoon, after all the fun is said and done, we will travel the same way back to Groningen and we hope you will take some amazing memories with you.	
+						</p>
+						<br>
+						<br>
+					</div>
+					<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 ">
+						<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2401.4175536205385!2d6.97653941582522!3d52.99487607990686!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47b7c5497949e8b5%3A0xb0e273677d48e28b!2sPagedal+Conferentiecentrum!5e0!3m2!1snl!2snl!4v1490020148711" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+					</div>
+				</div>
+				<hr>
+				<div class="row">
+					<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+						<!--<img src="images/thema.jpg" style="width:450px;" alt="Campingspel">-->
+						<iframe width="560" height="315" src="https://www.youtube.com/embed/92FbRmOfO58" frameborder="0" allowfullscreen></iframe>
+					</div>
+					<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+						<h3 class="pad">Theme</h3>
+						<p class="pad">
+							The location is about 30 miles from Groningen next to a lake, near a beautifull forest. The grass on our site is the brightest green we've ever seen and we're doing our best to get a nice weekend of sunshine for the camp. This years theme for the introduction camp is Heroes vs Villians.
+							<br>
+							<br>
+							Along with a title, every year's camp is build around a certain theme. And obviously the theme for this years' camp is Heroes vs Villians. This weekend you will meet the greatest heroes and the eviliest villians. From Batman to Superman and from the Joker to Ursula.
+						</p>
+					</div>
+				</div>
+			</div>
+			<hr>
+			<h1>Organisation</h1>
+			<div class="row navPadding" id="commissie">
+				<div class="col-lg-3 col-md-3 col-sm-4 col-xs-12">
+					<img src="images/commissie/KevinG.jpg" style="width:150px;" alt="Kevin">
+					<h3>Kevin Gevers<br> <small>Chairman</small></h3>
+					<p>
+						This is Kevin, the chairman of the IntroCee. He is a third year computing science student and is quite active within Cover. Since he will be on the board of Cover the upcoming year, you better make him your friend. Step 1: steal his hat. Step 2: run. Guaranteed besties. Besides being the chairman of the Introcee, he also is the chairman of the Activitee, so he has quite some experience within Cover which will come in handy next year. 
+					</p>
+				</div>
+				<div class="col-lg-3 col-md-3 col-sm-4 col-xs-12">
+					<img src="images/commissie/JoukeW.jpg" style="width:150px;" alt="Jouke">
+					<h3> Jouke van der Weij<br> <small> Vice-chairman </small></h3>
+					<p>
+						Jouke is our Vice-Chairman and is in the first year of his master. As the oldest member in our committee he has a lot of experience with introcamps, which comes in very handy. You can always find Jouke in the cover room if you need his help with something. But never start throwing beer caps at him, because it will start a war and you will lose for sure! In his free time Jouke likes to play games on his phone or play some dice games with other Cover members. He’s very excited about the camp and he hopes you all sign up!
+					</p>
+				</div>
+				<div class="col-lg-3 col-md-3 col-sm-4 col-xs-12">
+					<img src="images/commissie/NielsM.jpg" style="width:150px;" alt="Niels">
+					<h3>Niels Meima <br> <small>Secretary</small></h3>
+					<p>
+					This is Niels, he is our lovely, bearded, 20 year old enterpeneur. As a 3rd year AI student and owner of Meima Development, he has quite a lot of experience in both the fields AI and CS. Even though he might seem busy because of all this, he will make sure that he makes all of his deadlines and help others where needed. So do you need any help with your courses the coming years, don't be afraid to ask him to help you (speaking of, don't be afraid to approach anyone for help). Although he now may seem to be a workaholic, he sure liked to enjoy a cold one aswell. Just like Stijn, Niels also really enjoys beard scratches.
+				</div>
+				<div class="col-lg-3 col-md-3 col-sm-4 col-xs-12">
+					<img src="images/commissie/MariekeK.jpg" style="width:150px;" alt="Marieke">
+					<h3>Marieke Kopmels <br> <small>Treasurer</small></h3>
+					<p>
+					Marieke is the youngest member of this committee but definitely appears to be the most responsible which makes her extremely suitable for her job as the treasurer. She makes sure we’re not spending money impractically and have enough to feed everyone! When she’s not locked up in her room studying, Marieke likes to either meet up with her gyas buddies to row along the beautiful canals of Groningen or have some chill drinks with her housemates. But her favourite hobby of all is chugging beers, so don’t be afraid to challenge her!
+					</p>
+				</div>
+			</div> 	
+			<div class="row">
+				<div class="col-lg-3 col-md-3 col-sm-4 col-xs-12">
+					<img src="images/commissie/DaanL.jpg" style="width:150px;" alt="Daan">
+					<h3>Daan Lambert<br> <small>General member</small></h3>
+					<p>
+						Daan is a guy and in his second year of his AI bachelor. He is an active Cover member and is also a committee member of the SportTee. He also is the redhead of the IntroCee. We still have our souls, so do not hesitate to approach him at the IntroCamp! Instead of feeding himself with souls, he likes to cook. If you ever want to taste the best sushi in your life, go to Daan. He is like the best sushi masterchef we know of. He is looking forward to meeting y’all!
+					</p>
+				</div>
+				<div class="col-lg-3 col-md-3 col-sm-4 col-xs-12">
+					<img src="images/commissie/StijnK.jpg" style="width:150px;" alt="Stijn">
+					<h3>Stijn Kramer<br> <small>General member</small></h3>
+					<p>
+						Stijn is the current Treasurer of Cover. This means he has a lot to do, but he is always happy to have a chat. He already spend two years studying AI before becoming a member of the board. Stijn likes hardware and talking about computer components. He travels all over, mostly by train, sparingly by NS-replacement bus and on a rare occasion by helicopter. He can often be found going out, and enjoying a drink in the city. If he isn't busy with work for the board or getting board points, he likes doing absolutely nothing. He is very good at this. He absolutely adores beard scratches, so don't be afraid to go up to him and give his beard a scratch, no matter how annoyed he looks.
+					</p>
+				</div>
+				<div class="col-lg-3 col-md-3 col-sm-4 col-xs-12">
+					<img src="images/commissie/CarynnaZ.jpg" style="width:150px;" alt="Carynna">
+					<h3>Carynna de Zilva <br> <small>General member</small></h3>
+					<p>
+						Carynna is a first year student from Singapore. She is an enthousiastic girl, and just as Oscar trying to learn some Dutch words. Ofcourse as fellow committee members we couldn't help ourselves and tried to teach her some inappropriate words which we translated incorrectly. So far she could see right through our little play. 
+					</p>
+				</div>
+				<div class="col-lg-3 col-md-3 col-sm-4 col-xs-12">
+					<img src="images/commissie/Oscar.jpg" style="width:150px;" alt="Oscar">
+					<h3>Oscar Bocking<br> <small>General member</small></h3>
+					<p>
+						Oscar is a second year AI student from the UK. He is one of the last members to join the IntroCee, but that doesn't make him the least. At the moment he is trying to learn some dutch, and is always trying to learn new words. Maybe you can learn him some new words in your free time?
+					</p>
+				</div>
+			</div>
+			<hr>
+			<div class="row navPadding" id="paklijst">
+			<h1>Grablist</h1>
+				<div class="col-lg-6">
+					<h3>Sleeping</h3>
+					<ul class="list-group">
+						<li class="list-group-item">Pillow</li>
+						<li class="list-group-item">Sleeping bag or blanket cover </li>
+						<li class="list-group-item">Pyjamas</li>
+						<li class="list-group-item">Pillowcase</li>
+						<li class="list-group-item">Sheets</li>
+					</ul>
+				</div>
+				<div class="col-lg-6">
+					<h3>Clothing</h3>
+					<ul class="list-group">
+						<li class="list-group-item">Clothes for getting dirty</li>
+						<li class="list-group-item">Long pants</li>
+						<li class="list-group-item">Shorts</li>
+						<li class="list-group-item">Old shoes</li>
+						<li class="list-group-item">Socks</li>
+						<li class="list-group-item">Underwear</li>
+						<li class="list-group-item">Shirts</li>
+						<li class="list-group-item">Swimwear</li>
+						<li class="list-group-item">Warm jacket/hoodie</li>
+						<li class="list-group-item">(Garbage) bag for dirty clothes</li>
+					</ul>
+				</div>
+				<div class="col-lg-6">
+					<h3>Personal Care</h3>
+					<ul class="list-group">
+						<li class="list-group-item">Towel</li>
+						<li class="list-group-item">Toothbrush</li>
+						<li class="list-group-item">Toothpaste</li>
+						<li class="list-group-item">Soap/Shampoo</li>
+						<li class="list-group-item">Washcloth</li>
+						<li class="list-group-item">Deodorant</li>
+						<li class="list-group-item">Sun block</li>
+						<li class="list-group-item">Medicines</li>
+					</ul>
+				</div>
+				<div class="col-lg-6">
+					<h3>Others</h3>
+					<ul class="list-group">
+						<li class="list-group-item">ID</li>
+						<li class="list-group-item">Cutlery</li>
+						<li class="list-group-item">Plate</li>
+						<li class="list-group-item">Dishcloth</li>
+                                                <li class="list-group-item">Cutlery</li>
+						<li class="list-group-item">Phone and charger (optional)</li>
+						<li class="list-group-item">Drinking cup</li>
+						<li class="list-group-item">Water gun</li>
+						<li class="list-group-item">Something to do during breaks, like a book or games (optional)</li>
+						<li class="list-group-item">Cigarettes (optional, there is no option to buy them during the camp)</li>
+					</ul>
+				</div>
+			</div>
+			<hr>
+			<div class="row fH navPadding" id="contact">
+				<h1>Contact</h1>
+				<div class="jumbotron">
+					<h3>
+						<span class="glyphicon glyphicon-envelope"></span>
+						<small>introcee@svcover.nl</small>
+						<br>
+					
+						<span class="glyphicon glyphicon-phone"></span>
+						<small>Kevin Gevers: +31 6 48680791</small>
+					</h3>
+				</div>
+			</div>
+			<!-- Modal HTML -->
+			<div id="inschrijvingModal" class="modal fade">
+				<div class="modal-dialog">
+					<div class="modal-content">
+						<div class="modal-header">
+							<h3>Signup-form</h3>
+							</div>
+						<div class="modal-body">
+							<div class="tabbable"> <!-- Only required for left/right tabs -->
+								<ul class="nav nav-tabs">
+								<li class="active"><a href="#tab1" data-toggle="tab">New students</a></li>
+								<li><a href="#tab2" data-toggle="tab">Older-year students</a></li>
+								</ul>
+								<div class="tab-content">
+									<div class="tab-pane active" id="tab1"><br />
+										<div class ="alert alert-danger" role="alert">
+											All spots have been taken, if you sign up now, you will be placed on the waiting list.
+										</div>
+										<div class="alert alert-danger" role="alert">
+											NB. If you don't live in a SEPA country, or your BIC doesn't get accepted, contact us at <a href="mailto:introcee@svcover.nl">introcee@svcover.nl</a>
+										</div>
+										<?php include('forms/signup_form_foeten.php'); ?>
+									</div>
+									<div class="tab-pane" id="tab2"><br />
+										<div class="alert alert-danger" role="alert">
+											All spots have been taken, if you sign up now, you will be placed on the waiting list.
+										</div>
+										<?php include('forms/signup_form_niet_foeten.php'); ?>
+									</div>
+								</div>
+							</div>
+					   </div>
+					</div>
+				</div>
+			</div>
+		</div>
+<script>
+
+
+	$(".signup").on( "submit", function(event){
+		event.preventDefault();
+		$.ajax({
+			type: "POST",
+		url: "signupForm.php",
+		data: $('.signup').serialize(),
+			success: function(msg){
+				$(".signup").html(msg);
+			}
+		});
+	});
+		
+	$(".signup_older").on( "submit", function(event){
+		event.preventDefault();
+		$.ajax({
+			type: "POST",
+		url: "signupForm.php",
+		data: $('.signup_older').serialize(),
+			success: function(msg){
+				$(".signup_older").html(msg);
+			}
+		});
+	});
+		
+</script>
+	</body>
+</html>
