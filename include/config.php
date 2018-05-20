f726c2e00e19c34deaf8c988dc7109ee2e7c67d1
martijnluinstra <martijnluinstra@gmail.com>
2018-05-20 20:32:41 +0200

    Remove config.php

--- include/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('ADMIN_COMMITTEE', 'introcee');
-define('ADMIN_EMAIL', 'IntroCee Cover <introcee@svcover.nl>');
-
-define('SERVER_NAME', 'https://inctrocee.svcover.nl');
-define('COVER_APP', 'IntroCee');
-define('COVER_SECRET', '');
