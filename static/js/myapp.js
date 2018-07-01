09bd296fe165f5e5971df73830368e0e269685d3
Yannick Stoffers <y.stoffers@rug.nl>
2018-07-01 16:12:53 +0200

    Added functionality to committee photo.

--- static/js/myapp.js
+++ static/js/myapp.js
@@ -1,1 +1,14 @@
 /* Your custom js here */
+
+function changeImage()
+{
+    var image =  document.getElementById("imgClickAndChange");
+    if (image.getAttribute('src') == "/static/img/committee.jpg")
+    {
+        image.src = "/static/img/bier.gif";
+    }
+    else
+    {
+        image.src = "/static/img/committee.jpg";
+    }
+}
