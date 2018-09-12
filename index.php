f7042382edbfa38715c849f603de2b8eb9d4257e
Yannick Stoffers <y.stoffers@rug.nl>
2018-09-12 12:19:38 +0200

    Added movies to front page.

--- index.php
+++ index.php
@@ -3,4 +3,9 @@ require_once 'include/init.php';
 
 // Create and run home view
 $view = new TemplateView('index');
+$closed = True;
+if ($closed) {
+	$view = new TemplateView('closed');
+}
+
 $view->run();
